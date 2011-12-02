223bd627e9c85d3d3890f4badb3f081908c89918
Peter Hajas <peterhajas@gmail.com>
2011-12-01 23:36:41 -0500

    Added SCNewsItemViewDataSource, a provider of SCNewsItemView instances

---
+++ Scoop/SCNewsItemViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  SCNewsItemViewDataSource.m
+//  Scoop
+//
+//  Created by Peter Hajas on 12/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SCNewsItemViewDataSource.h"
+
+@implementation SCNewsItemViewDataSource
+
+@end
